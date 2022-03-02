91958e822578f69434699e54a97081740785ef3c
nat-1501 <natalisoares90@gmail.com>
2022-03-02 18:09:20 -0300

    Funções arrow

---
+++ foundation/anatomy-3.js
@@ -0,0 +1,18 @@
+const increment1 = function(n) {
+    return n + 1
+}  
+
+const increment2 = (n) => {
+    return n + 1
+}
+
+const increment3 = (n) => {
+    return n + 1
+}
+
+const increment4 = (n) =>  n + 1
+
+console.log(increment1(1))
+console.log(increment2(5))
+console.log(increment3(55))
+console.log(increment4(155))
