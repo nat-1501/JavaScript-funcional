d66ff08c4d48511c81bf67ae22a1df1e339c0bba
nat-1501 <natalisoares90@gmail.com>
2022-03-03 09:48:45 -0300

    FUNÇOES SEM PARAMETROS

---
+++ foundation/anatomy-4.js
@@ -0,0 +1,13 @@
+// Anonymous function 
+(function (a, b, c) {
+    let x = 3
+    console.log(`Result: ${a + b + c}`)
+    console.log(x)
+})(1, 2, 3);
+
+(function (a, b, c) {
+    let x = 300
+    console.log(`Result: ${a + b + c}`)
+    console.log(x)
+})(1, 100, 3);
+
